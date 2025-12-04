111ca87bff623b2d970196b188c1d922eb893f64
JP <jp@jp.nl>
2025-12-04 10:31:02 +0100

    Oefening 9 - DAL geimplementeerd. Communicatie werkt, nu moet de InstaFail de backend ook daadwerkelijk nog eens gaan gebruiken

--- InstaFail/InstaFail/InstaFail.Client/Dal/CommentDal.cs
+++ InstaFail/InstaFail/InstaFail.Client/Dal/CommentDal.cs
@@ -1,3 +1,4 @@
+using Flurl.Http;
 using Insta.Shared.Dal;
 using Insta.Shared.Entities;
 
@@ -5,19 +6,28 @@ namespace InstaFail.Client.Dal;
 
 public class CommentDal : ICommentDal
 {
-    public Task<IEnumerable<Comment>> GetAllForPhotoAsync(int photoId)
+    public async Task<IEnumerable<Comment>> GetAllForPhotoAsync(int photoId)
     {
-        throw new NotImplementedException();
+        Console.WriteLine("getting all");
+        return await $"https://localhost:7262/api/photos/{photoId}/comments"
+            .GetJsonAsync<IEnumerable<Comment>>();
     }
 
-    public Task<Comment> AddForPhotoAsync(int photoId, Comment comment)
+    public async Task<Comment> AddForPhotoAsync(int photoId, Comment comment)
     {
-        throw new NotImplementedException();
+        Console.WriteLine("adding");
+        
+        return await $"https://localhost:7262/api/photos/{photoId}/comments"
+            .PostJsonAsync(comment)
+            .ReceiveJson<Comment>();
     }
 
-    public Task<Comment> EditAsync(Comment comment)
+    public async Task<Comment> EditAsync(Comment comment)
     {
-        // throw new NotImplementedException();
-        return Task.FromResult(comment);
+        Console.WriteLine("editing");
+        
+        return await $"https://localhost:7262/api/photos/{comment.PhotoId}/comments/{comment.Id}"
+            .PutJsonAsync(comment)
+            .ReceiveJson<Comment>();
     }
 }
